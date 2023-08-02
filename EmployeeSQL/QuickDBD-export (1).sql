9753812769007e1caaa57b0b0c972d2d453e60ec
zinor3026 <tnordon18@gmail.com>
2023-08-01 18:15:01 -0700

    First

---
+++ EmployeeSQL/QuickDBD-export (1).sql
@@ -0,0 +1,84 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE Department (
+    dept_no VARCHAR   NOT NULL,
+    dept_name VARCHAR   NOT NULL,
+    CONSTRAINT pk_Department PRIMARY KEY (
+        dept_no
+     )
+);
+
+CREATE TABLE Dept_emp (
+	dept_emp_no SERIAL PRIMARY KEY,
+    emp_no INT   NOT NULL,
+    dept_no VARCHAR   NOT NULL
+);
+
+DROP TABLE Dept_manager
+
+CREATE TABLE Dept_manager (
+	dept_manager_no SERIAL PRIMARY KEY,
+	dept_no VARCHAR   NOT NULL,
+    emp_no INT   NOT NULL
+);
+
+CREATE TABLE Employees (
+    emp_no INT   NOT NULL,
+    emp_title_id VARCHAR   NOT NULL,
+    birth_date DATE   NOT NULL,
+    first_name VARCHAR   NOT NULL,
+    last_name VARCHAR   NOT NULL,
+    sex VARCHAR   NOT NULL,
+    hire_date DATE   NOT NULL,
+    CONSTRAINT pk_Employees PRIMARY KEY (
+        emp_no
+     )
+);
+
+CREATE TABLE Salaries (
+    emp_no INT   NOT NULL,
+    salary INT   NOT NULL,
+    CONSTRAINT pk_Salaries PRIMARY KEY (
+        emp_no
+     )
+);
+
+CREATE TABLE Titles (
+    title_id VARCHAR   NOT NULL,
+    title VARCHAR   NOT NULL,
+    CONSTRAINT pk_Titles PRIMARY KEY (
+        title_id
+     )
+);
+
+ALTER TABLE Dept_emp ADD CONSTRAINT fk_Dept_emp_emp_no FOREIGN KEY(emp_no)
+REFERENCES Employees (emp_no);
+
+ALTER TABLE Dept_emp ADD CONSTRAINT fk_Dept_emp_dept_no FOREIGN KEY(dept_no)
+REFERENCES Department (dept_no);
+
+ALTER TABLE Dept_manager ADD CONSTRAINT fk_Dept_manager_dept_no FOREIGN KEY(dept_no)
+REFERENCES Department (dept_no);
+
+ALTER TABLE Dept_manager ADD CONSTRAINT fk_Dept_manager_emp_no FOREIGN KEY(emp_no)
+REFERENCES Employees (emp_no);
+
+ALTER TABLE Employees ADD CONSTRAINT fk_Employees_emp_title_id FOREIGN KEY(emp_title_id)
+REFERENCES Titles (title_id);
+
+ALTER TABLE Salaries ADD CONSTRAINT fk_Salaries_emp_no FOREIGN KEY(emp_no)
+REFERENCES Employees (emp_no);
+
+SELECT * FROM Department;
+
+SELECT * FROM Dept_emp;
+
+SELECT * FROM Dept_manager;
+
+SELECT * FROM Employees;
+
+SELECT * FROM salaries;
+
+SELECT * FROM titles;
